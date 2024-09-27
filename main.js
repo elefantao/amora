549edd991af95e89732269e370ea73bd9024a0a2
elefantao <161314484+elefantao@users.noreply.github.com>
2024-09-27 08:53:32 -0300

    fght

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ function soma(num1,num2) {
 return num1+num2 ;
     
 }
-console.log(soma(5,3));
- function 
+console.log(soma(5,3))
+
+function areaQuadrado(lado) {
+    return lado**2 ;
+}
+console.log(areaQuadrado(6))
