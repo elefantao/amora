5843876702001c497a0c6c6843b5e390385632de
elefantao <161314484+elefantao@users.noreply.github.com>
2024-09-27 09:02:53 -0300

    ewfttf

--- main.js
+++ main.js
@@ -7,4 +7,11 @@ console.log(soma(5,3));
 function areaQuadrado(lado) {
     return lado**2 ;
 }
-console.log(areaQuadrado(6))
+console.log(areaQuadrado(6));
+
+let media=function (num1,num2,num3) {
+    return num1+num2+num3/3;
+
+
+}
+console.log(media(4,5,6))
