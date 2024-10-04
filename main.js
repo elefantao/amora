4c27aa450c8d9d2331454a63c2c239b815d7e5a0
elefantao <161314484+elefantao@users.noreply.github.com>
2024-10-04 09:03:07 -0300

    ytui

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ let estados=['Rio Grande do Sul', 'Santa Catarina', 'Paraná', 'Minas Gerais', '
 let i= 0;
 while (i<10){
     console.log(i);
-    console.çog(estados[i]);
+    console.log(estados[i]);
     i++;
-    
+
 
 
 }
