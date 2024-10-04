91402c0cbc2c0e5ce7124da96c0b77cc80a7957f
elefantao <161314484+elefantao@users.noreply.github.com>
2024-10-04 08:32:19 -0300

    vhm

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-function soma(num1,num2) {
-return num1+num2 ;
+// function soma(num1,num2) {
+// return num1+num2 ;
     
-}
-console.log(soma(5,3));
+// }
+// console.log(soma(5,3));
 
-function areaQuadrado(lado) {
-    return lado**2 ;
-}
-console.log(areaQuadrado(6));
+// function areaQuadrado(lado) {
+//     return lado**2 ;
+// }
+// console.log(areaQuadrado(6));
 
-let media=function (num1,num2,num3) {
-    return (num1+num2+num3)/3;
+// let media=function (num1,num2,num3) {
+//     return (num1+num2+num3)/3;
 
 
-}
-console.log(media(1,2,3));
+// }
+// console.log(media(1,2,3));
 
-let notas=parseInt(prompt('Notas Primeiro Tri'));
-let notas=parseInt(prompt('Notas Segundo Tri'))
-document.write()
+// let notas=parseInt(prompt('Notas Primeiro Tri'));
+// let notas=parseInt(prompt('Notas Segundo Tri'))
+// document.write()
 
-parseInt (prompt(()))
+// parseInt (prompt(()))
+
+let estados=['Rio Grande do Sul', 'Santa Catarina', 'Paraná', 'Minas Gerais', 'Rio de Janeiro', 'São Paulo', 'Espirito Santo', 'Ceará', 'Pará', 'Acre', 'Mato Grosso', 'Mato Grosso do Sul', 'Bahia', 'Goiás', 'Piauí', 'Amazonas', 'Maranhão', 'Pernambuco', 'Rio Grande do Norte', 'Sergipe', 'Alagoas', 'Rondônia', 'Roraima', 'Paraíba', 'Tocantins', 'Amapá', 'Distrito Federal'];
+
+console
+
